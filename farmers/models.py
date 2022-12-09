539e98bbfcc14d61464ac070b5668fea933269b7
Morces <karanim594@gmail.com>
2022-12-09 12:48:33 +0300

    created farmer and vendor models

--- farmers/models.py
+++ farmers/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
 # Create your models here.
+
+class Farmer(models.Model):
+    name= models.CharField(max_length = 200, null=True)
+    phone= models.CharField(max_length = 200, null=True)
+    email=models.CharField(max_length = 200, null=True)
+
+    def __str__(self):
+       return self.name
+
+class Customer(models.Model):
+    name= models.CharField(max_length = 200, null=True)
+    phone= models.CharField(max_length = 200, null=True)
+    email=models.CharField(max_length = 200, null=True)
+
+    def __str__(self):
+       return self.name
+
